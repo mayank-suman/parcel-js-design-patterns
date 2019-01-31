dc76a829ebe6db039d990656fa2888b445d0d4c4
mayank <mayankcsmn@gmail.com>
2019-01-31 08:30:09 +0530

    added module pattern files

--- src/index.js
+++ src/index.js
@@ -1,16 +1,1 @@
-var a = 1;
-
-(function module() {
-    var a = 2;
-    console.log(a);
-})();
-
-console.log(a);
-
-function fn() {
-    var a = 3;
-    console.log(a);
-}
-
-fn();
-console.log(a);
+import './module-pattern'
---
+++ src/module-pattern.js
@@ -0,0 +1,14 @@
+
+(function() {
+
+})();
+
+/* 
+Return an object instead of a function
+
+Advantages -  
+1. Encapsulation - protecting states and behaviors from being accessed from other classes.
+2, The module pattern allows for public and private (plus the lesser-know protected and privileged) access levels.
+
+
+*/
